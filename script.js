fbffa25c5df95c8d42e427b688e0854704a7d865
Ryan Victor <ryanvictor.dev@gmail.com>
2024-02-06 09:51:34 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // PEGAR A IMAGEM
+    const img = document.querySelector('#profile img')
+
+        //Substituir a imagem
+    if(html.classList.contains('light')) {
+    img.setAttribute('src', '/assets/avatar-light.png')
+    alt.setAttribute('alt','Foto do Ryan com fundo roxo')
+    } else {
+    img.setAttribute('src', '/assets/avatar.png')
+    alt.setAttribute('alt','Foto do Ryan com fundo branco')
+
+    }
+    
+    //Se tiver light mode, mudar para imagem clara
+    //Se tiver dark mode, manter a imagem normal
+}
